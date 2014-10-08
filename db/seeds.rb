fdc3faf815ed4ddf63a727874168cb4f05889f16
ka <ka.kaosf@gmail.com>
2014-10-09 01:13:59 +0900

    Create 3 schedules as seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Schedule.count < 3
+  Schedule.create([
+    {
+      start_at: '2014-10-10T00:00:00+0900',
+      end_at:   '2014-10-10T01:00:00+0900',
+      title: 'Title 1',
+    },
+    {
+      start_at: '2014-10-10T01:00:00+0900',
+      end_at:   '2014-10-10T02:00:00+0900',
+      title: 'Title 2',
+    },
+    {
+      start_at: '2014-10-10T02:00:00+0900',
+      end_at:   '2014-10-10T03:00:00+0900',
+      title: 'Title 3',
+    },
+  ])
+end
